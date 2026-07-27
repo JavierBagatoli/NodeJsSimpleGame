ae280cc002a9ce6224fd73c62ba2bdf11af01fa0
Javier Bagatoli <javi.bagatoli@gmail.com>
2026-07-27 18:59:17 -0300

    fix: firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,16 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "../serviceAccountKey.json";
+//import serviceAccount from "../serviceAccountKey.json";
 import { getFirestore } from 'firebase-admin/firestore';
 
 initializeApp({
-  credential: cert(serviceAccount as any),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
 });
+
 
 export const auth = getAuth();
 export const db = getFirestore();
